065d089095ec5e4c7d2ad6cc03b1146fe241d73c
Jeff <bra16024@byui.edu>
2020-05-05 23:55:00 -0500

    Added quantity to product types

--- littleMiss/script/main.js
+++ littleMiss/script/main.js
@@ -10,3 +10,12 @@ function hidemenu() {
         })
 
 }
+
+function showQtyInput(event) {
+    let element = document.getElementById(event.target.id + 'qty');
+    if (event.target.checked)
+        element.style.display = 'block';
+
+    else
+        element.style.display = 'none';
+}
